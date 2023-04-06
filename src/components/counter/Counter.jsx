331ed227755c48c2e9c24dedfa8ca4bdfba1b4ed
Marius CA <mariuskcodeacademy@gmail.com>
2023-04-06 11:52:03 +0300

    useReducer up ir down

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,28 +1,49 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import styled from 'styled-components';
 import Card from './../ui/card/Card';
 import { useState } from 'react';
 
-function Counter() {
-  const [state, setValue] = useState({ value: 0 });
+const initState = { value: 0 };
 
+// reducerio funkcija. komponento veiksmu logika ir eiga.
+// turi grazinti state busena kokia turi po pakeitimu
+function counterReducer(state, action) {
+  console.log('counterReducer ivyko');
+  console.log('state ===', state);
+  console.log('action ===', action);
+  if (action.type === 'UP') {
+    return { value: state.value + 1 };
+  } else if (action.type === 'DOWN') {
+    return { value: state.value - 1 };
+  }
+}
+
+function Counter() {
+  // const [state, setValue] = useState(initState);
+  const [state, dispach] = useReducer(counterReducer, initState);
   // fn handleUp
   function handleUp() {
-    setValue({ value: state.value + 1 });
+    // setValue({ value: state.value + 1 });
+    dispach({ type: 'UP' });
   }
 
   // fn hadnleDown
   function hadnleDown() {
-    setValue({ value: state.value - 1 });
+    // setValue({ value: state.value - 1 });
+    dispach({ type: 'DOWN' });
   }
+
+  // fn handleReset()
+  // padaryti kad reset nunulintu value
 
   return (
     <Card>
       <h2>Counter</h2>
-      <Value>{state.value}</Value>
+      <Value>{state?.value}</Value>
       <div className="control">
         <button onClick={handleUp}>Up</button>
         <button onClick={hadnleDown}>Down</button>
+        <button onClick={hadnleDown}>Reset</button>
       </div>
     </Card>
   );
